4c2155bfc43cdf85476a54d6597976b7c325417e
Keegan Leary <keegs@Keegans-iMac.local>
2020-05-21 22:20:59 -0700

    MuzzleFlash

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -9,6 +9,8 @@ public class Weapon : MonoBehaviour
     public LayerMask whatToHit;
 
     public Transform BulletTrailPrefab;
+    public Transform MuzzleFlashPrefab;
+
     float timeToSpawnEffect = 0;
     public float effectSpawnRate = 10;
     float timeToFire = 0;
@@ -56,5 +58,10 @@ public class Weapon : MonoBehaviour
 
     void Effect() {
         Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation);
+        Transform clone = Instantiate(MuzzleFlashPrefab, firePoint.position, firePoint.rotation) as Transform;
+        clone.parent = firePoint;
+        float size = Random.Range(0.6f, 0.9f);
+        clone.localScale = new Vector3(size, size, size);
+        Destroy(clone.gameObject, 0.02f);
     }
 }
