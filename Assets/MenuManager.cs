68f4e7bff576b6b7c083397eaf3b533316fa5ac1
Keegan Leary <leary.keegan@gmail.com>
2020-06-01 20:28:50 -0700

    Main Menu

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿// http://www.keeganleary.com
+// Copyright (c) Keegan Leary
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("We Quit the game.");
+        Application.Quit();
+    }
+}
